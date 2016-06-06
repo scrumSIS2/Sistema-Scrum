5e86b0af48a7f6fd3ae34be49b1e17fd142b6e4e
favioEvangelion <favio_evangelion@hotmail.com>
2016-06-06 01:02:28 -0400

    actualizacion base de datos
    esta es la base de datos que use

--- SistemaScrum/base de datos/scrumsistema.sql
+++ SistemaScrum/base de datos/scrumsistema.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : PRUEBAS
-Source Server Version : 50141
+Source Server         : localhost
+Source Server Version : 50505
 Source Host           : localhost:3306
 Source Database       : scrumsistema
 
 Target Server Type    : MYSQL
-Target Server Version : 50141
+Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2016-06-05 20:37:18
+Date: 2016-06-06 01:02:04
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,17 +23,18 @@ CREATE TABLE `historias` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(30) NOT NULL,
   `Yo_como` varchar(30) NOT NULL,
-  `Requiero` varchar(30) NOT NULL,
-  `Tal_que` varchar(30) NOT NULL,
-  `Condiciones_de_Satisfaccion` varchar(50) NOT NULL,
+  `Requiero` varchar(100) NOT NULL,
+  `Tal_que` varchar(100) NOT NULL,
+  `Condiciones_de_Satisfaccion` varchar(100) NOT NULL,
   `Importancia` int(11) NOT NULL,
   `Complejidad` int(11) NOT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of historias
 -- ----------------------------
+INSERT INTO `historias` VALUES ('4', 'Horarios', 'miembro', 'ver los horarios	', 'pueda estar al tanto de las reuniones', 'muy visible', '5', '4');
 
 -- ----------------------------
 -- Table structure for `horarios`
@@ -90,7 +91,6 @@ CREATE TABLE `hu_rechazadas` (
 -- ----------------------------
 -- Records of hu_rechazadas
 -- ----------------------------
-INSERT INTO `hu_rechazadas` VALUES ('1', 'Interfaz', 'miembro', 'ver mis trabajos', 'pueda organizarlos', 'debe poder ordenarse', '23', '12');
 
 -- ----------------------------
 -- Table structure for `info_usuarios`
@@ -103,35 +103,37 @@ CREATE TABLE `info_usuarios` (
   `telefono` int(11) NOT NULL,
   `email` varchar(40) NOT NULL,
   `direccion` varchar(40) NOT NULL,
-  `rol` varchar(30) NOT NULL,
+  `rol` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of info_usuarios
 -- ----------------------------
-INSERT INTO `info_usuarios` VALUES ('1', 'Juan Daniel', 'Sanchez Montaño', '60398621', 'juandanielsanchez20130@gmail.com', 'Av.Petrolera km 2.5', 'miembro');
-INSERT INTO `info_usuarios` VALUES ('2', 'Favio', 'Torihuano', '61619012', 'favio@hotmail.com', 'Quillacollo', 'scrummaster');
-INSERT INTO `info_usuarios` VALUES ('8', 'fruko', 'villarroel', '288339493', 'fruko@gmail.com', 'callee 566', 'miembro');
+INSERT INTO `info_usuarios` VALUES ('1', 'juan daniel', 'sanchez montaño', '60398621', 'juandanielsanchez20130@gmail.com', 'petrolera', 'miembro');
+INSERT INTO `info_usuarios` VALUES ('2', 'favio', 'torihuano', '61619012', 'favio.kaka@gmailk.com', 'ffdsacabajhghj', 'scrum_master');
+INSERT INTO `info_usuarios` VALUES ('5', 'Kevin ', 'Lopez', '72238321', 'kevin.caca@hotmail.com', 'Quillacollo 2.5', 'miembro');
+INSERT INTO `info_usuarios` VALUES ('9', 'frukogt', 'pendiente', '72255426', 'fryko.kaka@gmail.com', 'sacaba', 'miembro');
 
 -- ----------------------------
 -- Table structure for `login_usuarios`
 -- ----------------------------
 DROP TABLE IF EXISTS `login_usuarios`;
 CREATE TABLE `login_usuarios` (
-  `id_usuario` int(10) NOT NULL AUTO_INCREMENT,
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
   `usuario` varchar(30) NOT NULL,
-  `pass` varchar(30) NOT NULL,
+  `pass` int(11) NOT NULL,
   `tipo_usuario` varchar(20) NOT NULL,
-  PRIMARY KEY (`id_usuario`,`usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of login_usuarios
 -- ----------------------------
 INSERT INTO `login_usuarios` VALUES ('1', 'daniel', '123', 'miembro');
 INSERT INTO `login_usuarios` VALUES ('2', 'favio', '123', 'scrum_master');
-INSERT INTO `login_usuarios` VALUES ('8', 'fruko', '123', 'miembro');
+INSERT INTO `login_usuarios` VALUES ('5', 'Kevin', '123', 'miembro');
+INSERT INTO `login_usuarios` VALUES ('9', 'fruko', '123', 'miembro');
 
 -- ----------------------------
 -- Table structure for `tareas`
@@ -146,9 +148,8 @@ CREATE TABLE `tareas` (
   `id_historias` varchar(25) NOT NULL,
   PRIMARY KEY (`id_tareas`),
   KEY `id_historias` (`id_historias`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of tareas
 -- ----------------------------
-INSERT INTO `tareas` VALUES ('2', 'holas', 'holas tareas', 'to do', null, '1');
